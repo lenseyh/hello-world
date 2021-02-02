21cc5a89fbb469223bd75b3c1618638be73c1bcd
MONAD\Lensey.Hau <lensey.hau@moneris.com>
2021-02-02 13:26:47 -0800

    First commit

---
+++ SplitFiles/SplitFiles/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace SplitFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSplit = new System.Windows.Forms.Button();
+            this.txtfileSelected = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtNumberOfFiles = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSplit
+            // 
+            this.btnSplit.Location = new System.Drawing.Point(47, 165);
+            this.btnSplit.Name = "btnSplit";
+            this.btnSplit.Size = new System.Drawing.Size(75, 23);
+            this.btnSplit.TabIndex = 0;
+            this.btnSplit.Text = "Split File";
+            this.btnSplit.UseVisualStyleBackColor = true;
+            this.btnSplit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtfileSelected
+            // 
+            this.txtfileSelected.Location = new System.Drawing.Point(47, 54);
+            this.txtfileSelected.Name = "txtfileSelected";
+            this.txtfileSelected.Size = new System.Drawing.Size(298, 20);
+            this.txtfileSelected.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(369, 54);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // txtNumberOfFiles
+            // 
+            this.txtNumberOfFiles.Location = new System.Drawing.Point(47, 116);
+            this.txtNumberOfFiles.Name = "txtNumberOfFiles";
+            this.txtNumberOfFiles.Size = new System.Drawing.Size(69, 20);
+            this.txtNumberOfFiles.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Select File";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Number of files";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(249, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 247);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumberOfFiles);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtfileSelected);
+            this.Controls.Add(this.btnSplit);
+            this.Name = "Form1";
+            this.Text = "Split File";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSplit;
+        private System.Windows.Forms.TextBox txtfileSelected;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txtNumberOfFiles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SplitFiles/SplitFiles/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SplitFiles
+{
+    public partial class Form1 : Form
+    {
+        int numberOfFile = 10;
+        String fileFullName;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fileFullName = txtfileSelected.Text;
+            numberOfFile = int.Parse(txtNumberOfFiles.Text);
+            long fileLength = new FileInfo(fileFullName).Length / numberOfFile;
+            String filePath = Path.GetDirectoryName(fileFullName);
+
+            SplitFile(fileFullName, Convert.ToInt32(fileLength) + 1, filePath);
+            
+            label3.Text = "Completed!";
+        }
+
+        public static void SplitFile(string inputFile, int chunkSize, string path)
+        {
+            byte[] buffer = new byte[chunkSize];
+            string fileBaseName = inputFile.Substring(path.Length+1);
+            using (Stream input = File.OpenRead(inputFile))
+            {
+                int index = 0;
+                while (input.Position < input.Length)
+                {
+                    using (Stream output = File.Create(path + "\\" + fileBaseName + "_" + index))
+                    {
+                        int chunkBytesRead = 0;
+                        while (chunkBytesRead < chunkSize)
+                        {
+                            int bytesRead = input.Read(buffer,
+                                                       chunkBytesRead,
+                                                       chunkSize - chunkBytesRead);
+
+                            if (bytesRead == 0)
+                            {
+                                break;
+                            }
+                            chunkBytesRead += bytesRead;
+                        }
+                        output.Write(buffer, 0, chunkBytesRead);
+                    }
+                    index++;
+                }
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            txtfileSelected.Text = openFileDialog1.FileName;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtNumberOfFiles.Text = "10";
+        }
+    }
+}
